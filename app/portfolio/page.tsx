5a42d12bf1399bb7325d89ca023d14a4622657f0
Tehran Bayramov <nicosspyros@gmail.com>
2025-02-06 16:08:15 +0400

    revalidate added

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,11 +1,19 @@
+import next from 'next';
 import PortfolioHero from '../components/portfolioPage/PortfolioHero';
 import ProjectGrid from '../components/portfolioPage/ProjectGrid';
 
 
 
 export default async function PortfolioPage() {
-    const response = await fetch("https://api.samramprojects.com/api/projects?populate=*");
+    const response = await fetch("https://api.samramprojects.com/api/projects?populate=*",
+        {
+            next: {
+                revalidate: 10
+            }
+        }
+    );
     const projectsData = await response.json();
+
 
     return (
         <>
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -3,8 +3,15 @@ import ServicesList from '../components/servicesPage/ServicesList';
 import ProcessSection from '../components/servicesPage/ProcessSection';
 
 export default async function ServicesPage() {
-    const services = await fetch("https://api.samramprojects.com/api/services?populate=*");
+    const services = await fetch("https://api.samramprojects.com/api/services?populate=*",
+        {
+            next: {
+                revalidate: 10
+            }
+        }
+    );
     const servicesData = await services.json();
+
     return (
         <>
             <ServicesHero />
